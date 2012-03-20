e1ce3f8d36e489c34ea5e1b5f9ea27f1396089cd
Neo Chen <neoxen@qq.com>
2012-03-20 17:26:20 +0800

    Added features for editing, deleting users, including protection against self-deletion

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,6 @@
 class Admin::UsersController < Admin::BaseController
+  before_filter :find_user, :only => [:show, :edit, :update, :destroy]
+  
   def index
     @users = User.all(:order => "username")
   end
@@ -7,9 +9,32 @@ class Admin::UsersController < Admin::BaseController
     @user = User.new
   end
   
+  def show
+    
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    if params[:user][:password].blank?
+      params[:user].delete(:password)
+      params[:user].delete(:password_confirmation)
+    end
+    set_admin
+    if @user.update_attributes(params[:user])
+      flash[:notice] = "User has been updated."
+      redirect_to admin_users_path
+    else
+      flash[:alert] = "User has not been updated."
+      render :action => "edit"
+    end
+  end
+  
   def create 
     @user = User.new(params[:user])
-    @user.admin = params[:user][:admin] == "1"
+    set_admin
     if @user.save
       flash[:notice] = "User has been created."
       redirect_to admin_users_path
@@ -19,7 +44,21 @@ class Admin::UsersController < Admin::BaseController
     end
   end
   
-  def to_s
-    "#{usernmae} (#{admin? ? "Admin" : "User"})"
+  def destroy
+    if @user == current_user
+      flash[:alert] = "You cannot delete yourself!"
+    else
+      @user.destroy
+      flash[:notice] = "User has been deleted."
+    end
+    redirect_to admin_users_path
+  end
+  
+  private
+  def find_user
+    @user = User.find(params[:id])
+  end
+  def set_admin
+    @user.admin = params[:user][:admin] == "1"
   end
 end

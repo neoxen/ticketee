779b9d7ef23216c52a3b4ea9c5f62d027c8abe6b
Neo Chen <neoxen@qq.com>
2012-03-21 17:03:50 +0800

    Made projects only visible to users with permission to see them

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,7 @@
 class ProjectsController < ApplicationController
   before_filter :authorize_admin!, :except => [:show, :index]
+  before_filter :authenticate_user!, :only => [:show]
+  before_filter :find_project, :only => [:show, :edit, :update, :destroy]
 
   def index
     @projects = Project.all
@@ -33,5 +35,13 @@ class ProjectsController < ApplicationController
 
   def show
     @project = Project.find(params[:id])
+  end
+  
+  private
+  def find_project
+    @project = Project.readable_by(current_user).find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = "The project you were looking for could not be found."
+    redirect_to projects_path
   end
 end
